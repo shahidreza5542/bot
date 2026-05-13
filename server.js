050d06d7cc13c71a6dfcf2d825f85b64924fe736
shahidreza <shahidreza660@gmail.com>
2026-05-13 21:14:14 +0530

    bug fix

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ app.use(cors({
   origin: process.env.CLIENT_URL || 'http://localhost:3000',
   credentials: true
 }));
+app.set('trust proxy', 1)
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
